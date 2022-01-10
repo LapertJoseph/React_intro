f893457f3b6ad622b37e114dedbd48f5ff4a4431
LapertJoseph <lapertjoseph@gmail.com>
2022-01-10 17:28:24 +0100

    Ajout comment

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from 'react';  // on importe les hooks useRef et useState (déclare un état=données et founis aussi la fonction qui modifie l'état)
 
 import Button from '../../components/Button';
 
@@ -14,28 +14,28 @@ const Todo = () => {
         { id: 4, texte: "Todo 4" }     // <li>Todo 4</li>
     ]);
 
-    const textRef = useRef();
+    const textRef = useRef();           // je crée une référence qui prévois de ratacher a input texte afin de récup la valeur 
 
-    const supprimer = (id) => {
+    const supprimer = (id) => {     // on utilise setTodos afin de filtrer le tableau todos via la fonction filtrer
         setTodos((prevState) => prevState.filter((todo) => todo.id !== id))
     }
     
     const ajouter = () => {
-        const newTodo = { id: todos.length + 1, texte: textRef.current.value }
+        const newTodo = { id: todos.length + 1, texte: textRef.current.value }  // ic on récupére la valeur de textRef afin de l'affecter à la nouvelle todo
         // setTodos([...todos, newTodo])
-        setTodos(prevState => [...prevState, newTodo])
+        setTodos(prevState => [...prevState, newTodo])          // ici on ajoute une todo via setTodos
     }
 
-    return ( 
+    return (                            // on effectue le rendu de la page todo
         <div className="todo">
             <div>
-                <input type="text" ref={textRef}/>
-                <Button texte="Ajouter" onClick={ajouter}></Button>
+                <input type="text" ref={textRef}/>                          {/* on rattache la ref textRef à l'élément input texte */}
+                <Button texte="Ajouter" onClick={ajouter}></Button>         {/* on attache la fonction ajouter à l'élément Button via l'évent Onclick */}
             </div>
             <ul>
-                {todos.map((todo) => (
-                    <li id={`todo-${todo.id}`} key={`todo-${todo.id}`}>
-                        {todo.texte} <Button onClick={() => supprimer(todo.id)} texte="supprimer"></Button>
+                {todos.map((todo) => (                                      // on utilise la fonction map afin de parcourir les todos et de retourner l'affichage attendu (<li> text <Button texte="Supprimer"/> <li> 
+                    <li id={`todo-${todo.id}`} key={`todo-${todo.id}`}>                                     {/* key dois être unique, nécéssaire à React (MAJ les rendus ) */}
+                        {todo.texte} <Button onClick={() => supprimer(todo.id)} texte="supprimer"></Button> 
                     </li>
                 ))}
             </ul>
