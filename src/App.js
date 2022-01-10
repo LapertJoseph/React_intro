217062653c8115788732da0f877e59a3d58a2247
LapertJoseph <lapertjoseph@gmail.com>
2022-01-10 10:56:19 +0100

    Ajout commentaires sur App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Todo from './pages/Todo';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import './App.css';
-
-
 
 const App = () => {    // Destructuring qui permet de récuperer les props de App.js
 
@@ -12,6 +10,9 @@ console.log();
   return (
     <div className="app">
       <Router>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
         <Routes>
           <Route path="/" element={ <Todo /> } />
           <Route path="/about" element={ <About /> } />
@@ -23,10 +24,12 @@ console.log();
 }
 
 export default App;
+// -----------------------------------------  Commentaire  ----------------------------------------------------------------------------------------//
 
-// une props sont des parametres (props, ) (le premier sera toujours des props) callback
-// quand on fais un rendu visuel on le met dans le return
-//la logique du composannt est avant le return
-// ne pas oublier d'importer le component dans App.js
-// import de BrowserRouter as Router et Routes as Switch et Route pour les rendu des components.
-// Ajout des route Todo, About et Erreur
+// 1 - une props sont des parametres (props, ) (le premier sera toujours des props) callback
+// 2 - quand on fais un rendu visuel on le met dans le return
+// 3 - la logique du composannt est avant le return
+// 4 - ne pas oublier d'importer le component dans App.js
+// 5 - import de BrowserRouter as Router et Routes as Switch et Route pour les rendu des components.
+// 6 - Ajout des route Todo, About et Erreur
+// 7 - Ajout Balise nav et link vers Home
