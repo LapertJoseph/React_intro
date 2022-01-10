1f10a6315074615132beda0f794bca2ec8b2b254
LapertJoseph <lapertjoseph@gmail.com>
2022-01-10 14:37:01 +0100

    ajout d'une liste avec la méthode map

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -1,15 +1,25 @@
 import "./index.css"
-import Button from "../Button"
-const Todo = () => {
+// import Button from "../Button"
 
-    
 
-    return ( 
-        <div className="todo" >
-            Todo <br />
-            <Button texte="Mon Bouton"/>
-        </div>
-     );
-}
+    const Todo = () => {
+
+        const todos = [
+            { id: 1, texte: "Todo 1" },
+            { id: 2, texte: "Todo 2" },
+            { id: 3, texte: "Todo 3" },
+            { id: 4, texte: "Todo 4" }
+        ]
+        
+        return ( 
+            <div className="todo">
+                <ul>
+                    {todos.map((todo) => <li>{todo.texte}</li>)}  {/* Permet de boucler dans le tableau en ajouter todo.texte au nouveau tableau créer par map*/}
+                </ul>
+            </div>
+        );
+    }
  
 export default Todo;
+
+// .map renvois un nouveau tableau avec la modification apportée et on peux construire des éléments ou des composants.
