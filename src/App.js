13bc98f7e864a0edbcdfd91af326f06171d18a5e
LapertJoseph <lapertjoseph@gmail.com>
2022-01-10 10:15:18 +0100

    clean fichier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 
-function App({ message }) {    // Destructuring qui permet de récuperer les props de App.js
+
+
+
+const App = () => {    // Destructuring qui permet de récuperer les props de App.js
 
 console.log();
   return (
-    <div className="App">
-  {message}
+    <div className="app">
+
     </div>
   );
 }
 
 export default App;
 
-// une props sont des parametres (props, ) (le premier sera toujours props)
+// une props sont des parametres (props, ) (le premier sera toujours des props) callback
+// quand on fais un rendu visuel on le met dans le return
+//la logique du composannt est avant le return
+// ne pas oublier d'importer le component dans App.js
