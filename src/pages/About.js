65a41a37fa46f0655fb975758c64095d3261edf4
LapertJoseph <lapertjoseph@gmail.com>
2022-01-10 10:18:50 +0100

    clean fichier

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+const About = () => {
+
+
+
+
+
+    return (
+        <div className="about">
+            À propos
+        </div>
+    );
+};
+
+export default About;
+
+//sfc = sans le nom du fichier
+// <> = permet de faire plusieurs balises à la suite
+// classeName au lieu de class car c'est du JSX
---
+++ src/pages/Todo.js
@@ -0,0 +1,14 @@
+const Todo = () => {
+
+
+
+
+    
+    return ( 
+        <div className="todo" >
+        Todo
+        </div>
+     );
+}
+ 
+export default Todo;
