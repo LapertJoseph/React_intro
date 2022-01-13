28316cb1167a3b29314f668c90847862646319b1
Joseph <Lapertjoseph@gmail.com>
2022-01-13 09:55:49 +0100

    Ajout d'un composant compteur et rendu sur la Todo

---
+++ src/components/Compteur/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            Total: {count}
+            <button onClick={() => setCount(0)}>Réinitialiser</button>
+            <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+        </div>
+    );
+};
+
+export default Counter;
