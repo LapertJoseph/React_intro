929fa03d4c41e24560697f857b8fd5089866b4a4
Joseph <Lapertjoseph@gmail.com>
2022-01-13 13:33:40 +0100

    modification des commentaire dans le fichier et ajout d'un paragraphe

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,13 +3,14 @@ const About = () => {
 
     return (
         <div className="about">
-            À propos
+            <h1>À propos</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet tenetur nemo quaerat magni deserunt, laudantium quod et officia minus vitae, nobis quia officiis earum quam sunt. Neque ex sint voluptas repellat vitae error nam? Tenetur, inventore omnis vel odit mollitia voluptas nulla molestiae, suscipit natus temporibus amet culpa modi exercitationem. Saepe deserunt ipsa, animi officiis tempore veritatis facere nobis libero, iure commodi vitae quae tempora dignissimos atque fugiat sapiente molestiae harum, perferendis consequatur expedita repellat? Quisquam doloribus reiciendis ipsum ut pariatur impedit totam provident minus? Dolorem nisi, ipsa rerum laboriosam eaque perferendis illo, voluptatem id repellat quo error hic voluptatibus impedit. Quod sapiente hic soluta dolore libero tenetur praesentium. Magni quidem maiores unde nostrum dolorem rem ab tempore vel natus.</p>
         </div>
     );
 };
 
 export default About;
 
-//sfc = sans le nom du fichier
-// <> = permet de faire plusieurs balises à la suite
+//rsc = créer un template de composant
+// <> = frament qui permet de faire plusieurs balises à la suite
 // classeName au lieu de class car c'est du JSX
